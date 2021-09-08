4592b135a3487a929f25aa376622f3321436eb15
xiaohei-maker <2977588071@qq.com>
2021-09-08 21:28:23 +0800

    最新的更新

--- src/main/java/com/example/demo/Controller/IndexController.java
+++ src/main/java/com/example/demo/Controller/IndexController.java
@@ -5,21 +5,27 @@ import com.example.demo.Service.QuestionService;
 import com.example.demo.Service.UserService;
 import com.example.demo.cache.HotTagCache;
 import com.example.demo.dto.PaginationDTO;
-import com.mysql.cj.Session;
+
+import com.example.uitils.Base64Utils;
+import com.example.uitils.EmailUtils;
+import com.example.uitils.RandomUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+
+
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.Random;
 import java.util.UUID;
+
+import static java.lang.Math.*;
 
 @Controller
 public class IndexController {
@@ -64,6 +70,7 @@ public class IndexController {
                         @RequestParam(name = "search", required = false) String search,
                         @RequestParam(name = "tag", required = false) String tag,
                         @RequestParam(name = "sort", required = false) String sort) {
+        HttpSession session=request.getSession();
         PaginationDTO pagination = questionService.list(search, tag, sort, page, size);
         List<String> tags = hotTagCache.getHots();
         model.addAttribute("pagination", pagination);
@@ -76,7 +83,7 @@ public class IndexController {
         String token= UUID.randomUUID().toString();
         String file=request.getParameter("file");
         String username=request.getParameter("username");
-        String password=request.getParameter("password1");
+        String password=request.getParameter("upassword1");
         String email=request.getParameter("email");
         String sex=request.getParameter("usex");
 
@@ -85,12 +92,159 @@ public class IndexController {
         user.setPassword(password);
         user.setEmail(email);
         user.setSex(sex);
+        user.setAccountId(username);
         user.setToken(token);
-        userService.createUser(user);
-        HttpSession session=request.getSession();
+        user.setCode(RandomUtils.createActive());
 
-        response.addCookie(new Cookie("token",token));
-      //  session.setAttribute("user",user);
-        return "redirect:/";
+//        String b="https://tupian.qqw21.com/article/UploadPic/2014-9/201492111331720507.jpg";
+//        String c="https://tupian.qqw21.com/article/UploadPic/2014-9/201492111332416492.jpg";
+//        String d="https://tupian.qqw21.com/article/UploadPic/2014-9/201492111331422951.jpg";
+        String b="http://pic.uuhy.com/uploads/2012/06/26/emma-watson-celebrity.jpg";
+        String c="http://pic.uuhy.com/uploads/2012/06/26/emma-watson-actress.jpg";
+        String d="http://pic.uuhy.com/uploads/2012/06/26/actress-emma-watson.jpg";
+        String  s[]=new  String[2];
+        Random random=new Random();
+        Integer  f= random.nextInt(3);
+        user.setAvatarUrl(s[f]);
+
+        user.setStatus(0);
+
+        String a=userService.createUser(user);
+        if(a==null){
+            return "error";
+        }else {
+            //session.setAttribute("userall",user);
+            response.addCookie(new Cookie("token",token));
+            return "registerSuccess";
+        }
+    }
+
+    //激活
+    @RequestMapping("/jihuo&c={code}")
+    //@ResponseBody
+    public  String active(@PathVariable(name = "code") String code,
+                          HttpServletRequest request,
+                          HttpServletResponse response) throws SQLException {
+        //String codes=request.getParameter("c");
+        String c= Base64Utils.decode(code);
+        //String c= String.valueOf(Base64Utils.decodeFromString(request.getParameter("c")));
+        Integer i=userService.selectCode(c);
+        if(i==0){
+            request.setAttribute("msg","激活失败！");
+            // return Constants.FORWARD+"/message.jsp";
+        }else if(i==1){
+            request.setAttribute("msg","激活成功！");
+            // return Constants.FORWARD+"/message.jsp";
+        }else {
+            request.setAttribute("msg","已经激活！");
+            // return Constants.FORWARD+"/message.jsp";
+        }
+        //3.响应
+        //request.getParameter("registerMsg")
+        return "denglu";
+    }
+
+    //登录功能
+    @GetMapping("/denglu")
+    public  String denglu(){
+        return  "denglu";
+    }
+
+    //登录功能
+    @RequestMapping(value = "/log")
+//    @ResponseBody
+    public String denglu(
+                         HttpServletRequest request,
+                         HttpServletResponse response,
+                         Model model,
+//                         String ins,
+//                         String ua,
+//                         String pd,
+//                         String cd,
+//                         String auto,
+                         @RequestParam(name = "page", defaultValue = "1") Integer page,
+                         @RequestParam(name = "size", defaultValue = "10") Integer size,
+                         @RequestParam(name = "search", required = false) String search,
+                         @RequestParam(name = "tag", required = false) String tag,
+                         @RequestParam(name = "sort", required = false) String sort) {
+        PaginationDTO pagination = questionService.list(search, tag, sort, page, size);
+        List<String> tags = hotTagCache.getHots();
+        model.addAttribute("pagination", pagination);
+        model.addAttribute("search", search);
+        model.addAttribute("tag", tag);
+        model.addAttribute("tags", tags);
+        model.addAttribute("sort", sort);
+        String token= UUID.randomUUID().toString();
+        String username=request.getParameter("uname");
+        String password=request.getParameter("password");
+        String auto = request.getParameter("auto");
+        String code = request.getParameter("code");
+        String code1 = request.getParameter("code1");
+        HttpSession session=request.getSession();
+        //String codes = ins.toLowerCase();
+        //String code = ins;
+        if (code.equals(code1)) {
+            User user = userService.LoginUser(username);
+                        if (user == null) {
+                            return "denglu";
+                        } else {
+                            if(user.getStatus().equals(0)){
+                                EmailUtils.sendEmail(user);
+                                return  "registerSuccess";
+                            }else {
+                                session.setAttribute("loginUser", user);
+                                // Cookie cookie = new Cookie("autoUser", Base64Utils.encode(username + ":" + password));
+                                Cookie cookie=new Cookie("token",user.getToken());
+                                cookie.setMaxAge(60 * 60 * 24 * 14);
+                                response.addCookie(cookie);
+                                return "redirect:/";
+                            }
+                        }
+                    }else{
+                        return "denglu";
+
+
+        }
+
+        //                if (auto == null) {
+        //                    //String name=session.user.g;
+        //                   // Cookie cookie=new Cookie("token",null);
+        //                    //response.addCookie(new Cookie("token",token));
+        //                    Cookie cookie=new Cookie("token",user.getToken());
+        //                    response.addCookie(cookie);
+        //                    return "redirect:/";
+        //                } else {
+        //                    // Cookie cookie = new Cookie("autoUser", Base64Utils.encode(username + ":" + password));
+        //                    Cookie cookie=new Cookie("token",user.getToken());
+        //                    cookie.setMaxAge(60 * 60 * 24 * 14);
+        //                    response.addCookie(cookie);
+        //                    return "redirect:/";
+        //                }
+
+
+
+
+//            User user = userService.LoginUser(username);
+//            if (user == null) {
+//                return "denglu";
+//            } else {
+//                session.setAttribute("loginUser", user);
+//
+//                    // Cookie cookie = new Cookie("autoUser", Base64Utils.encode(username + ":" + password));
+//                    Cookie cookie=new Cookie("token",user.getToken());
+//                    cookie.setMaxAge(60 * 60 * 24 * 14);
+//                    response.addCookie(cookie);
+//                    return "index";
+//
+//            }
+
+    }
+
+    @RequestMapping("/r")
+    @ResponseBody
+    public String r(HttpServletRequest request, HttpServletResponse response) {
+        String username = request.getParameter("username");
+        String a=userService.selectUsername(username);
+        return a;
     }
 }
--- src/main/java/com/example/demo/Service/UserService.java
+++ src/main/java/com/example/demo/Service/UserService.java
@@ -3,6 +3,7 @@ package com.example.demo.Service;
 import com.example.demo.Mapper.UserMapper;
 import com.example.demo.Model.User;
 import com.example.demo.Model.UserExample;
+import com.example.uitils.EmailUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -40,18 +41,84 @@ public class UserService {
 
     public String createUser(User user) {
         UserExample userExample = new UserExample();
-        userExample.createCriteria()
-                .andAccountIdEqualTo(user.getAccountId());
+        userExample.createCriteria().
+                andNameEqualTo(user.getName());
         List<User> users = userMapper.selectByExample(userExample);
-
+        EmailUtils.sendEmail(user);
+        if(users.size() != 0){
+            return  null;
+        }
         user.setGmtCreate(System.currentTimeMillis());
         user.setGmtModified(user.getGmtCreate());
-        userMapper.insert(user);
+        user.setStatus(0);
+        String a= String.valueOf(userMapper.insert(user));
+        return  a;
+    }
+
+
+
+
+    public User LoginUser(String username) {
+        UserExample userExample = new UserExample();
+        userExample.createCriteria().
+                andNameEqualTo(username);
+        List<User> users = userMapper.selectByExample(userExample);
+        for (User user1:users) {
+            if(user1.getName().equals(username)){
+                if (user1.getStatus()==null){
+                    UserExample example = new UserExample();
+                    user1.setStatus(1);
+                    example.createCriteria()
+                            .andIdEqualTo(user1.getId());
+                    userMapper.updateByExampleSelective(user1, example);
+                }
+                if (user1.getStatus().equals(0)){
+                    return user1;
+                }else {
+                    UserExample example = new UserExample();
+                    user1.setGmtModified(System.currentTimeMillis());
+                    example.createCriteria()
+                            .andIdEqualTo(user1.getId());
+                    userMapper.updateByExampleSelective(user1, example);
+                    return  user1;
+                }
+            }
+        }
         return  null;
     }
 
-//    //平常的登录和修改
-//    public String createUser(){
-//
-//    }
+    public Integer selectCode(String c) {
+        UserExample userExample = new UserExample();
+        userExample.createCriteria().
+                andCodeEqualTo(c);
+        List<User> users = userMapper.selectByExample(userExample);
+        if(users.size() != 0){
+            for (User user1:users) {
+                if(user1.getCode().equals(c)){
+                    //更新
+                    UserExample example = new UserExample();
+                    user1.setStatus(1);
+                    example.createCriteria()
+                            .andIdEqualTo(user1.getId());
+                    userMapper.updateByExampleSelective(user1, example);
+                    return  1;
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    public String selectUsername(String username) {
+        UserExample userExample = new UserExample();
+        userExample.createCriteria().
+                andNameEqualTo(username);
+        List<User> users = userMapper.selectByExample(userExample);
+        if(users.size() != 0){
+            return  "1";
+        }else {
+            return "0";
+        }
+
+    }
 }
